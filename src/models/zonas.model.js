096c97c355a3139db13e62c0c88e5d174c9c8eeb
GermanHernando <hernandoger@hotmail.com>
2024-12-14 19:12:39 -0300

    carga de listas

---
+++ src/models/zonas.model.js
@@ -0,0 +1,29 @@
+const resultados = [
+{
+    distrito: "zona1",
+    candidato: "cadidatoA"
+},
+{
+    distrito: "zona2",
+    candidato: "cadidatoB"
+},
+{
+    distrito: "zona3",
+    candidato: "cadidatoC"
+}
+]
+
+const candidatosValidos = [
+    {
+        candidato:"candidatoA"
+    }
+    {
+        candidato:"candidatoB"
+    },
+    {
+        candidato:"candidatoC"
+    },
+    {
+        candidato:"en blanco"
+    }
+]
